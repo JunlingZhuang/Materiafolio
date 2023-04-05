4aa487e7734e266beb2815b52e0bdfd75edff187
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2023-04-05 01:31:19 -0400

    update

---
+++ firstapp/src/Components/Nav.js
@@ -0,0 +1,14 @@
+function Nac(){
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
